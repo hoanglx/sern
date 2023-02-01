f4b6a8aa88fa4c32b016c3db6ac5e0daa1945a1b
hoanglx <hoanglx@gmail.com>
2023-02-01 13:37:21 +0700

    commit dau tien

---
+++ src/seeders/seeder-users.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "Le",
+        lastName: "Hoang",
+        address: "USA",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
